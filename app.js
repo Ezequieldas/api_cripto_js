dbce0f7fc88daf98347289c193bb09ac24d31fa6
Ezequiel Dastolfo <ezequieldastolfo@gmail.com>
2022-08-22 18:56:24 -0500

    fixed button, BEM

--- app.js
+++ app.js
@@ -15,16 +15,15 @@ const cryptoOptions = async () => {
 };
 
 const quoteCurrency = () => {
+  
   const currency=document.querySelector("#currency").value;
   const crypto=document.querySelector("#cryptocurrency").value;
 
   if (currency === '' || crypto === '') {
-    setTimeout (() => {
-    showError("#msj-error","ESCOGE DOS OPCIONES PARA COMPARAR");
-    return;},3000)
+    showError("#form__error","Seleccione ambas monedas para comparar...");
+    return;
   }
   
-
   quote(currency, crypto);
 }
 
@@ -33,26 +32,25 @@ const quote = async (currency, crypto) => {
   
   const response = await fetch (url);
   let results = await response.json();
-
+  
   results = results.DISPLAY[crypto][currency];
-  // console.log(results)
 
   let divResults=document.querySelector('#divResults');
 
   divResults.innerHTML = `<div style="text-align:center"><img src="assets/loading.gif" width=50 height=50></div>`;
 
   setTimeout(() => {
-    divResults.innerHTML= `<div class="precio"> El precio es: <span>${results.PRICE}</span></div>
-    <div class="info"> El precio más alto del día es: <span>${results.HIGHDAY}</span></div>
-    <div class="info"> El precio más bajo del día es: <span>${results.LOWDAY}</span></div>
-    <div class="info"> Variación últimas 24 horas: <span>${results.CHANGEPCT24HOUR}</span></div>
-    <div class="info"> Última actualización: <span>${results.LASTUPDATE}</span></div>
+    divResults.innerHTML= `<div class="form__price"> El precio es: <span>${results.PRICE}</span></div>
+    <div class="form__info"> El precio más alto del día es: <span>${results.HIGHDAY}</span></div>
+    <div class="form__info"> El precio más bajo del día es: <span>${results.LOWDAY}</span></div>
+    <div class="form__info"> Variación últimas 24 horas: <span>${results.CHANGEPCT24HOUR}</span></div>
+    <div class="form__info"> Última actualización: <span>${results.LASTUPDATE}</span></div>
     `;
   },2000);
 }
 
 const showError = (elements, message) => {
   divError = document.querySelector(elements);
-  divError.innerHTML=`<p class="red darken-4 error">${message}</p>`;
+  divError.innerHTML=`<p class="form__error">${message}</p>`;
   setTimeout(() => { divError.innerHTML=``;},2000);
 }
